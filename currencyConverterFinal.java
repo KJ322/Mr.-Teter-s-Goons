2a767f81eb5cc64005d10ba780bd31daa4bacdc3
Isaiah McNeill <mcneilli4402@student.faytechcc.edu>
2025-04-10 16:57:55 -0400

    Update currencyConverterFinal.java

--- currencyConverterFinal.java
+++ currencyConverterFinal.java
@@ -7,29 +7,30 @@ public class currencyConverterFinal
     {
         //initializing arrays    
         String[] currencies = {
-            "Afghan Afghani (AFN)", "Algerian Dinar (DZD)", "Angolan Kwanza (AOA)", "Bahraini Dinar (BHD)",
-            "Botswana Pula (BWP)", "Burundian Franc (BIF)", "Cape Verdean Escudo (CVE)", "Central African CFA Franc (XAF)",
-            "Comorian Franc (KMF)", "Congolese Franc (CDF)", "Djiboutian Franc (DJF)", "Egyptian Pound (EGP)",
-            "Eritrean Nakfa (ERN)", "Ethiopian Birr (ETB)", "Gambian Dalasi (GMD)", "Ghanaian Cedi (GHS)",
-            "Guinean Franc (GNF)", "Iranian Rial (IRR)", "Iraqi Dinar (IQD)", "Israeli New Shekel (ILS)",
-            "Jordanian Dinar (JOD)", "Kenyan Shilling (KES)", "Kuwaiti Dinar (KWD)", "Lebanese Pound (LBP)",
-            "Lesotho Loti (LSL)", "Liberian Dollar (LRD)", "Libyan Dinar (LYD)", "Malagasy Ariary (MGA)",
-            "Malawian Kwacha (MWK)", "Mauritanian Ouguiya (MRU)", "Mauritian Rupee (MUR)", "Moroccan Dirham (MAD)",
-            "Mozambican Metical (MZN)", "Namibian Dollar (NAD)", "Nigerian Naira (NGN)", "Omani Rial (OMR)",
-            "Qatari Riyal (QAR)", "Rwandan Franc (RWF)", "Sao Tomean Dobra (STN)", "Saudi Riyal (SAR)",
-            "Seychellois Rupee (SCR)", "Sierra Leonean Leone (SLL)", "Somali Shilling (SOS)", "South African Rand (ZAR)",
-            "South Sudanese Pound (SSP)", "Swazi Lilangeni (SZL)", "Syrian Pound (SYP)", "Tunisian Dinar (TND)",
-            "U.S. Dollar (USD)", "United Arab Emirates Dirham (AED)", "West African CFA Franc (XOF)", "Zimbabwean Dollar (ZWL)"
+             "Afghan Afghani (AFN)", "Algerian Dinar (DZD)", "Angolan Kwanza (AOA)", "Bahraini Dinar (BHD)", 
+             "Botswana Pula (BWP)", "Burundian Franc (BIF)", "Cape Verdean Escudo (CVE)", "Central African CFA Franc (XAF)", 
+             "Comorian Franc (KMF)", "Congolese Franc (CDF)", "Djiboutian Franc (DJF)", "Egyptian Pound (EGP)", 
+             "Eritrean Nakfa (ERN)", "Ethiopian Birr (ETB)", "Gambian Dalasi (GMD)", "Ghanaian Cedi (GHS)", 
+             "Guinean Franc (GNF)", "Iranian Rial (IRR)", "Iraqi Dinar (IQD)", "Israeli New Shekel (ILS)", 
+             "Jordanian Dinar (JOD)", "Kenyan Shilling (KES)", "Kuwaiti Dinar (KWD)", "Kyrgyzstan Som (KGS)", 
+             "Lebanese Pound (LBP)", "Lesotho Loti (LSL)", "Liberian Dollar (LRD)", "Libyan Dinar (LYD)", 
+             "Malagasy Ariary (MGA)", "Malawian Kwacha (MWK)", "Mauritanian Ouguiya (MRU)", "Mauritian Rupee (MUR)", 
+             "Moroccan Dirham (MAD)", "Mozambican Metical (MZN)", "Namibian Dollar (NAD)", "Nigerian Naira (NGN)", 
+             "Omani Rial (OMR)", "Pakistani Rupee (PKR)", "Qatari Riyal (QAR)", "Rwandan Franc (RWF)", 
+             "Saudi Riyal (SAR)", "Sao Tomean Dobra (STN)", "Seychellois Rupee (SCR)", "Sierra Leonean Leone (SLL)", 
+             "Somali Shilling (SOS)", "South African Rand (ZAR)", "South Sudanese Pound (SSP)", "Sudanese Pound (SDG)", 
+             "Swazi Lilangeni (SZL)", "Syrian Pound (SYP)", "Tajikistan Somoni (TJS)", "Tanzanian Shilling (TZS)", 
+             "Turkish Lira (TRY)", "Turkmenistan Manat (TMT)", "Tunisian Dinar (TND)", "U.S. Dollar (USD)", 
+             "Ugandan Shilling (UGX)", "United Arab Emirates Dirham (AED)", "Uzbekistan Som (UZS)", 
+             "West African CFA Franc (XOF)", "Yemeni Rial (YER)", "Zambian Kwacha (ZMW)", "Zimbabwean Dollar (ZWL)"
         };
         
         double[] rates = {
-            71.60, 132.83, 63.8874, 0.376, 56.50, 2965.90, 99.77, 603.00,
-            0.385, 1410.25, 177.94, 47.50, 15.00, 8676.76, 568.50, 15.49,
-            130.26, 42112.50, 1309.88, 3.70, 0.709, 129.25, 0.30, 89596.90,
-            601.00, 46.00, 4.82, 4642.89, 1733.99, 39.88, 64.75, 9.50,
-            2686.45, 18.80, 1537.39, 19.2786, 3.64, 3660.86, 22.64, 3.75,
-            921.163, 20969.50, 21.00, 18.76, 250.35, 2906.08, 13000.00, 3.06,
-            1.00, 3.67, 594.94, 361.90
+            71.60, 132.83, 19.2786, 0.376, 18.80, 2965.90, 99.77, 603.00, 56.50, 63.8874, 177.94, 47.50, 
+            15.00, 2906.08, 63.8874, 15.49, 601.00, 42112.50, 1309.88, 3.70, 0.709, 129.25, 0.30, 84.36, 
+            89596.90, 130.26, 46.00, 4.82, 2965.90, 1733.99, 39.88, 64.75, 9.50, 1410.25, 18.76, 1537.39, 
+            0.385, 280.00, 3.64, 601.00, 3.75, 22.64, 56.50, 20969.50, 2686.45, 18.76, 3660.86, 21.00, 
+            0.385, 13000.00, 10.938, 250.35, 36.68, 3.50, 3.06, 1.0, 568.50, 3.67, 12930.00, 594.94, 36.68, 361.90
         };
 
         //creating the frame
